8e4f33f4122322efa777af20f6182429580b44f8
Shane Harper <cdtharper@gmail.com>
2021-03-01 21:54:28 -0500

    add random number generator for a random number between 1 and 10

--- TortoiseHare.java
+++ TortoiseHare.java
@@ -1,5 +1,7 @@
+import java.lang.Math;
+
 public class TortoiseHare {
     public static void main(String[] args) {
-        System.out.println("Hello world.");
+        int randomNumber = (int) ( 10 * Math.random() ) + 1;
     }
 }
